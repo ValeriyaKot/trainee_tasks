1dcd3f74c4746545543533353c58c606eaa61b2d
Valeriya <valeric.kot988@gmail.com>
2021-02-09 13:11:33 +0300

    create converter

--- feature/converter/converter.py
+++ feature/converter/converter.py
@@ -1,10 +1,23 @@
 import json
 import csv
 
+# my_table = [["first_name", "second_name", "Grade"],
+#           ['Alex', 'Brian', 'A'],
+#           ['Tom', 'Smith', 'B']]
+#
+# my_file = open('test.csv', 'w')
+# with my_file:
+#     writer = csv.writer(my_file)
+#     writer.writerows(my_table)
 
+res = []
 with open('test.csv') as f:
     reader = csv.DictReader(f)
-    rows = list(reader)
+    for row in reader:
+        res.append(row)
+    print(res)
 
 with open('test.json', 'w') as f:
-    json.dump(rows, f)
+    json.dump(res, f)
+
+
