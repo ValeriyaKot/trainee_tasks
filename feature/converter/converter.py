10559a87cac1dbe665f495c94f12fc7ee8581532
Valeriya <valeric.kot988@gmail.com>
2021-02-10 19:37:20 +0300

    create class Converter, create a new file for work with files

--- feature/converter/converter.py
+++ feature/converter/converter.py
@@ -1,23 +1,32 @@
-import json
-import csv
+class Converter:
 
-# my_table = [["first_name", "second_name", "Grade"],
-#           ['Alex', 'Brian', 'A'],
-#           ['Tom', 'Smith', 'B']]
-#
-# my_file = open('test.csv', 'w')
-# with my_file:
-#     writer = csv.writer(my_file)
-#     writer.writerows(my_table)
+    def __init__(self, text):
+        self.text = text
 
-res = []
-with open('test.csv') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        res.append(row)
-    print(res)
+    def split_text(self):
+        new_text = []
+        for element in self.text:
+            element = element.strip('\r')
+            string = element.split(',')
+            new_text.append(string)
+        return new_text
 
-with open('test.json', 'w') as f:
-    json.dump(res, f)
+    def join_text(self):
+        new_text = self.split_text()
+        all_text = []
+        for j in range(1, len(new_text)):
+            if len(new_text[j]) <= 1:
+                continue
+            else:
+                row = {}
+                for i in range(len(new_text[0])):
+                    row[new_text[0][i]] = new_text[j][i]
+                all_text.append('{%s}' % ', '.join(['"{}":"{}"'.format(k, v) for k, v in row.items()]))
+                self.text = all_text
+        return self.text
+
+
+
+
 
 
