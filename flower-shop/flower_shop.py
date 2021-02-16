220cdae94f799c282f57973548fadd9cddefe0d5
Valeriya <valeric.kot988@gmail.com>
2021-02-16 14:09:05 +0300

    add magic methods, refactoring code

--- flower-shop/flower_shop.py
+++ flower-shop/flower_shop.py
@@ -1,67 +1,93 @@
-flowers_list = []
-bouquets = []
-order_list = []
+from enum import Enum, auto
+from collections import defaultdict
+
+
+class FlowerType(Enum):
+    Rose = auto()
+    Aster = auto()
+    Tulip = auto()
+    Lily = auto()
+    Iris = auto()
 
 
 class Flower:
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
+    def __init__(self, flower_type):
+        self.flower_type = flower_type
 
-    def save_flower(self):
-        flower = {'type': self.name, 'color': self.color}
-        flowers_list.append(flower)
+    def __str__(self):
+        return 'flower type: {}'.format(self.flower_type)
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.flower_type == other.flower_type
 
 
 class Bouquet:
-    def __init__(self, name, price, quantity, *flower_types):
+    def __init__(self, name, price, flowers=None):
         self.name = name
         self.price = price
-        self.quantity = quantity
-        self.flower_types = flower_types
+        self.flowers = (defaultdict(int, flowers)
+                        if flowers is not None
+                        else defaultdict(int))
 
-    def create_bouquet(self):
-        bouquet = []
-        for flower in flowers_list:
-            for flower_type in self.flower_types:
-                if flower['type'] == flower_type:
-                    bouquet.append(flower)
-        bouquet_dict = {'bouquet_name': self.name, 'quantity': self.quantity, 'price': self.price, 'bouquet': bouquet}
-        bouquets.append(bouquet_dict)
+    def __str__(self):
+        return 'Bouquet: {}'.format(self.name)
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __getitem__(self, key):
+        return self.flowers[key]
+
+    def __setitem__(self, key, value):
+        self.flowers[key] = value
 
 
 class Shop:
-    def __init__(self, bouquet_name, ordered_quantity):
-        self.bouquet_name = bouquet_name
-        self.ordered_quantity = ordered_quantity
+    def __init__(self, shop_name, bouquets=None):
+        self.shop_name = shop_name
+        self.bouquets = (defaultdict(int, bouquets)
+                         if bouquets is not None
+                         else defaultdict(int))
 
-    def sell(self):
-        for bouquet in bouquets:
-            if bouquet['bouquet_name'] != self.bouquet_name:
-                continue
-            else:
-                if bouquet['quantity'] >= self.ordered_quantity:
-                    bouquet['quantity'] -= self.ordered_quantity
-                else:
-                    self.order()
+    def __str__(self):
+        return self.shop_name
 
-    def order(self):
-        order = {'bouquet_name': self.bouquet_name, 'ordered_quantity': self.ordered_quantity}
-        order_list.append(order)
+    def __getitem__(self, key):
+        return self.bouquets[key]
+
+    def __setitem__(self, key, value):
+        self.bouquets[key] = value
+
+    def check_quantity_of_bouquets(self, bouquet, quantity):
+        if self.bouquets[bouquet] < quantity:
+            raise ValueError('Not enough bouquets')
+
+    def sell(self, bouquet, quantity):
+        self.check_quantity_of_bouquets(bouquet, quantity)
+        self.bouquets[bouquet] -= quantity
+
+    def order(self, bouquet, quantity):
+        self.bouquets[bouquet] += quantity
 
 
 if __name__ == "__main__":
-    rose = Flower('rose', 'pink')
-    lily = Flower('lily', 'white')
+    rose = Flower(FlowerType.Rose)
+    aster = Flower(FlowerType.Aster)
+    lily = Flower(FlowerType.Lily)
+    tulip = Flower(FlowerType.Tulip)
+    iris = Flower(FlowerType.Iris)
 
-    rose.save_flower()
-    lily.save_flower()
+    fantastic = Bouquet('fantastic', 1.25, {rose: 20})
+    romantic = Bouquet('romantic', 1.50, {tulip: 5, rose: 20})
 
-    fantastic = Bouquet('fantastic', 1.25, 5, 'rose', 'lily')
-    romantic = Bouquet('romantic', 1.50, 10, 'lily')
+    shop = Shop('Iris', {fantastic: 10, romantic: 2})
 
-    fantastic.create_bouquet()
-    romantic.create_bouquet()
-
-    flower_shop = Shop('fantastic', 2)
-    flower_shop.sell()
+    shop.sell(fantastic, 2)
+    shop.sell(romantic, 1)
+    shop.order(romantic, 2)
